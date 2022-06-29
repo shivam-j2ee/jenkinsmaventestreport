f00ddf3b16a423fe426961646e63d97c551a1699
shivam-j2ee <tiwari.shivam26@gmail.com>
2022-06-30 03:05:43 +0530

    second commit

--- src/test/java/com/simplilearn/project2/EvenOddTest.java
+++ src/test/java/com/simplilearn/project2/EvenOddTest.java
@@ -1,10 +1,13 @@
 package com.simplilearn.project2;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class EvenOddTest {
 	
 	@Test
